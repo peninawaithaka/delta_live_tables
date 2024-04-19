f61a5182d4c77d8f5135401196e264b55d47e208
Penina Waithaka <peninawaithaka5@gmail.com>
2024-04-19 08:25:42 +0300

    loading the sales data

--- test/Learn/test.sql
+++ test/Learn/test.sql
@@ -1,11 +1,15 @@
---Task
--- Source data from the databricks cloud file
---Create a DLT pipeline
-
---Task 1
---Loading raw customers data into a streaming table
-CREATE STREAMING LIVE TABLE customers
+--Load the raw customers data into a streaming live table
+CREATE STREAMING LIVE TABLE raw_customers
 AS 
 SELECT 
   * 
 FROM cloud_files("/databricks-datasets/retail-org/customers/", "csv");
+
+
+--Load the raw sales data from the databricks sample data
+CREATE STREAMING LIVE TABLE raw_sales
+AS 
+SELECT 
+  customer_id,
+  customer_name
+FROM cloud_files("/databricks-datasets/retail-org/sales_orders/");
